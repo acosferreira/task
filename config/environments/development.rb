771cbc9b850e75ac03f566c868cfbf9c4827ebd9
acosferreira <acosferreira@gmail.com>
2013-07-26 22:09:55 +0200

    add email to send reminder, configure devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Task::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #to devise authentication
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
