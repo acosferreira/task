e3691b9a2dd52c92c2bbb72a65bd7bca3dc2c83f
acosferreira <acosferreira@gmail.com>
2013-07-27 18:46:40 +0200

    add validation before delete recurrency and type task

--- app/models/recurrency.rb
+++ app/models/recurrency.rb
@@ -1,5 +1,13 @@
 class Recurrency < ActiveRecord::Base
   attr_accessible :name
+  has_many :jobs
   validates :name, :presence=>true
   validates :name, :uniqueness=>true
+
+  before_destroy :is_delete_possible?
+
+  def is_delete_possible?
+  	return true if self.jobs.blank?
+  	return false
+  end
 end
--- app/models/type_task.rb
+++ app/models/type_task.rb
@@ -1,5 +1,15 @@
 class TypeTask < ActiveRecord::Base
   attr_accessible :name
+  has_many :jobs
   validates :name, :presence=>true
   validates :name, :uniqueness=>true
+
+
+  before_destroy :is_delete_possible?
+
+  def is_delete_possible?
+  	return true if self.jobs.blank?
+  	return false
+  end
+  
 end
